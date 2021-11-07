a4f18fa63cf4e6d52aeec31e0ee8b04ec84882b2
Tyler Casperson <tylercasperson@gmail.com>
2021-11-07 01:43:56 -0600

    able to connect to the database

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
-import express from 'express';
-
+const express = require('express');
+const cors = require('cors');
 const app = express();
+const db = require('./models');
 
 const PORT = process.env.PORT || 5000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
 
 app.listen(PORT, console.log(`Server running on ${PORT}.`));
