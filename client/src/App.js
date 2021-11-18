7e90c96baa4136e22200e23ad7041464344d1a2b
Tyler Casperson <tylercasperson@gmail.com>
2021-11-17 23:22:32 -0700

    added a navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Navbar from './components/layout/Navbar';
 import Forecast from './components/pages/Forecast';
 
 function App() {
   return (
     <div className='App'>
+      <Navbar />
       <Forecast />
     </div>
   );
---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div style={{ marginBottom: '20px' }}>
+      <ul>
+        <li>
+          <a href='#!'>Line Chart</a>
+        </li>
+        <li>
+          <a href='#!'>Forecast Types</a>
+        </li>
+        <li>
+          <a href='#!'>Time Periods</a>
+        </li>
+        <li>
+          <a href='#!'>Forecast Setup</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
