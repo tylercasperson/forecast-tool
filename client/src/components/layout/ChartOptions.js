7dbf93ff7b73a72edd9bd0fed3de06cbd02ee176
Tyler Casperson <tylercasperson@gmail.com>
2021-12-09 00:14:11 -0700

    able to hide the chart options with the click of an icon

---
+++ client/src/components/layout/ChartOptions.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import ToggleSwitch from './ToggleSwitch';
+
+const ChartOptions = (props) => {
+  return (
+    <div
+      style={{
+        display: props.display,
+        flexDirection: 'column',
+        marginTop: '10px',
+        marginBottom: '10px',
+      }}
+    >
+      <div style={{ display: 'flex', width: '70%', flexBasis: '100%', padding: '1vh' }}>
+        <b style={{ marginTop: '-10px' }}>Chart Options</b>
+        <ToggleSwitch
+          label={'Change Colors'}
+          onChange={props.changeColors}
+          name={'changeColors'}
+          checked={props.showChangeColors}
+        />
+        <ToggleSwitch
+          label={props.showHideHoverText}
+          onChange={props.onChange}
+          name={'showHideHoverLabels'}
+          checked={props.showHoverLabels}
+        />
+      </div>
+      <div
+        style={{
+          display: 'flex',
+          flexBasis: '100%',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+        }}
+      >
+        <b style={{ marginTop: '-10px' }}>Show/Hide Data?</b>
+        <ToggleSwitch
+          label={'Sales History'}
+          onChange={props.onChange}
+          checked={props.showSalesHistory}
+          name={'salesHistory'}
+        />
+        <ToggleSwitch
+          label={'Last Year'}
+          onChange={props.onChange}
+          checked={props.showLastYear}
+          name={'lastYear'}
+        />
+        <ToggleSwitch
+          label={'Moving Average'}
+          onChange={props.onChange}
+          checked={props.showMovingAverage}
+          name={'movingAverage'}
+        />
+        <ToggleSwitch
+          label={'Weighted Average'}
+          onChange={props.onChange}
+          checked={props.showWeightedAverage}
+          name={'weightedAverage'}
+        />
+        <ToggleSwitch
+          label={'Linear Regression'}
+          onChange={props.onChange}
+          checked={props.showLinearRegression}
+          name={'linearRegression'}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ChartOptions;
--- client/src/components/layout/ShowHide.js
+++ client/src/components/layout/ShowHide.js
@@ -1,66 +1,31 @@
-import React from 'react';
-import ToggleSwitch from './ToggleSwitch';
+import React, { useState } from 'react';
+import ChartOptions from './ChartOptions';
 
 const ShowHide = (props) => {
+  const [display, setDisplay] = useState('none');
+  const onClick = () => {
+    if (display === 'none') {
+      setDisplay('flex');
+    } else {
+      setDisplay('none');
+    }
+  };
   return (
-    <div
-      style={{ display: 'flex', flexDirection: 'column', marginTop: '10px', marginBottom: '10px' }}
-    >
-      <div style={{ display: 'flex', width: '70%', flexBasis: '100%', padding: '1vh' }}>
-        <b style={{ marginTop: '-10px' }}>Chart Options</b>
-        <ToggleSwitch
-          label={'Change Colors'}
-          onChange={props.changeColors}
-          name={'changeColors'}
-          checked={props.showChangeColors}
-        />
-        <ToggleSwitch
-          label={props.showHideHoverText}
-          onChange={props.onChange}
-          name={'showHideHoverLabels'}
-          checked={props.showHoverLabels}
-        />
-      </div>
-      <div
-        style={{
-          display: 'flex',
-          flexBasis: '100%',
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-        }}
-      >
-        <b style={{ marginTop: '-10px' }}>Show/Hide Data?</b>
-        <ToggleSwitch
-          label={'Sales History'}
-          onChange={props.onChange}
-          checked={props.showSalesHistory}
-          name={'salesHistory'}
-        />
-        <ToggleSwitch
-          label={'Last Year'}
-          onChange={props.onChange}
-          checked={props.showLastYear}
-          name={'lastYear'}
-        />
-        <ToggleSwitch
-          label={'Moving Average'}
-          onChange={props.onChange}
-          checked={props.showMovingAverage}
-          name={'movingAverage'}
-        />
-        <ToggleSwitch
-          label={'Weighted Average'}
-          onChange={props.onChange}
-          checked={props.showWeightedAverage}
-          name={'weightedAverage'}
-        />
-        <ToggleSwitch
-          label={'Linear Regression'}
-          onChange={props.onChange}
-          checked={props.showLinearRegression}
-          name={'linearRegression'}
-        />
-      </div>
+    <div style={{ margin: ' -2.5vh 0 0 0.5vw' }}>
+      <i onClick={() => onClick()} className='fas fa-cog'></i>
+      <ChartOptions
+        display={display}
+        onChange={props.onChange}
+        showSalesHistory={props.showSalesHistory}
+        showLastYear={props.showLastYear}
+        showMovingAverage={props.showMovingAverage}
+        showWeightedAverage={props.showWeightedAverage}
+        showLinearRegression={props.showLinearRegression}
+        changeColors={props.changeColors}
+        showChangeColors={props.showChangeColors}
+        showHoverLabels={props.showHoverLabels}
+        showHideHoverText={props.showHideHoverText}
+      />
     </div>
   );
 };
