80241c038190a4b1b62658479d9229342fb22e90
Tyler Casperson <tylercasperson@gmail.com>
2021-12-23 02:55:24 -0700

    when clicked, the days not of the month will move the calendar forward or backwards

--- client/src/components/layout/calendar/DropDownCalendar.js
+++ client/src/components/layout/calendar/DropDownCalendar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { add, format } from 'date-fns';
 
@@ -112,6 +112,12 @@ const DropDownCalendar = (props) => {
     setMonthTracker([0, 0]);
     setDisplayStartDate(startDate);
   };
+
+  useEffect(() => {
+    setMonthTracker([0, 0]);
+    setDisplayStartDate(startDate);
+    setDisplayEndDate(endDate);
+  }, [startDate, endDate]);
 
   return (
     <div
